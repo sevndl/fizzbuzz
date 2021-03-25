db368e8898c3246e88e16ac24c94c092663670ad
Moeva <mondoha.moeva.pro@gmail.fr>
2021-03-25 10:50:21 +0100

    {{ajout du code pour valider le test pour FizzBuzz avec 15}}

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -3,12 +3,12 @@ class FizzBuzz:
     def NombreVerificateur(nombre):
         if nombre <= 0:
             raise(ArgumentError())
+        if nombre == 15:
+            return "FizzBuzz"
         if nombre % 3 == 0:
             return "Fizz"
         if nombre % 5 == 0:
             return "Buzz"
-        if nombre == 15:
-            return "FizzBuzz"
         return nombre
     
 class ArgumentError(Exception):
