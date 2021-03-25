e79ba5c5ef3b10759c890a7352e351f725e3dadf
sevndl <nandillons@gmail.com>
2021-03-25 10:12:26 +0100

    Refacto tests renvoient une erreur si le nombre est nul ou négatif

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,9 +1,7 @@
 class FizzBuzz:
     @staticmethod
     def NombreVerificateur(nombre):
-        if nombre == 0 or nombre == -1:
-            raise(ArgumentError())
-        elif nombre < 0:
+        if nombre <= 0:
             raise(ArgumentError())
         return nombre
     
