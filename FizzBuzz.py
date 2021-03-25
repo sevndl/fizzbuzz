24b9650927efccf94e10c7565d458599ebf0454f
sevndl <nandillons@gmail.com>
2021-03-25 10:11:54 +0100

    Validation du test avec un nombre négatif

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -3,6 +3,8 @@ class FizzBuzz:
     def NombreVerificateur(nombre):
         if nombre == 0 or nombre == -1:
             raise(ArgumentError())
+        elif nombre < 0:
+            raise(ArgumentError())
         return nombre
     
 class ArgumentError(Exception):
