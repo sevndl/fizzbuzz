79cddc6a9ebe5e7d777fce1be59d29f7d04f3edd
Moeva <mondoha.moeva.pro@gmail.fr>
2021-03-25 09:51:06 +0100

    {{add test_renvoie_le_Nombre}}

--- FizzBuzz_test.py
+++ FizzBuzz_test.py
@@ -7,3 +7,8 @@ class FizzBuzz_test(unittest.TestCase):
         nombre = 4
         nombreRendu = FizzBuzz.NombreVerificateur(nombre)
         self.assertEqual(nombre,nombreRendu)
+
+    def test_renvoie_le_nombre(self):
+        nombre = 6
+        nombreRendu = FizzBuzz.NombreVerificateur(nombre)
+        self.assertEqual(nombre,nombreRendu)
