75207dec90dfd3b20afb409dfd3d1da3c34d8670
sevndl <nandillons@gmail.com>
2021-03-26 17:56:36 +0100

    Ajout du code pour faire passer ce test, les autres cassent

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,6 +1,10 @@
 class FizzBuzz:
     @staticmethod
     def NombreVerificateur(nombre):
+        if nombre.isalpha():
+            raise(ArgumentError('PAS DE LETTRES'))
+        else:
+            nombre = int(nombre)
         if nombre <= 0:
             raise(ArgumentError('Le nombre doit être positif.')) 
         if nombre % 5 == 0 and nombre % 3 == 0:
